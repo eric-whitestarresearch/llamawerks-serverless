a044882ada8ac86122be866aa9548fe56f5613b4
Eric Moody <Eric Moody>
2024-11-14 14:28:09 -0600

    Fix logger not logging in cloud watch

--- code/service_engine/code/modules/logger.py
+++ code/service_engine/code/modules/logger.py
@@ -4,6 +4,8 @@ from os import environ
 
 
 if environ.get('LOG_LEVEL') == 'DEBUG':
-  logging.basicConfig(stream=stdout, level=logging.DEBUG)
+  logging.basicConfig(stream=stdout)
+  logging.getLogger().setLevel(logging.DEBUG)
 else:
-  logging.basicConfig(stream=stdout, level=logging.INFO)
+  logging.basicConfig(stream=stdout)
+  logging.getLogger().setLevel(logging.INFO)
